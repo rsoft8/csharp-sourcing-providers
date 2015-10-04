a7e031e0fcd49d7487242df955571a08cc0201df
Ryan <kuchanryan@yahoo.com>
2015-10-04 00:00:26 -0400

    Initial commit

---
+++ SourcingProviderTests/HomeAdvisorTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace FcSoftware.SourcingProviderTests
+{
+    public class HomeAdvisorTests
+    {
+        [Fact]
+        public void GetServiceTypes()
+        {
+            
+        }
+    }
+}
