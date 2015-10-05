c576eb8e76206c0f131a6514633b471728d35671
Ryan <kuchanryan@yahoo.com>
2015-10-05 15:35:40 -0400

    Commenting LoadServices

--- SourcingProviders/HomeAdvisor/HomeAdvisor.cs
+++ SourcingProviders/HomeAdvisor/HomeAdvisor.cs
@@ -11,19 +11,27 @@ namespace FcSoftware.SourcingProviders.HomeAdvisor
         public async Task<List<Service>> LoadServices()
         {
             List<Service> services = null;
+
+            // URL with dropdown box of categories
             var url = @"http://www.homeadvisor.com/c.html";
 
+            // Grab the source of the page
             using (var client = new HttpClient())
             using (var response = await client.GetAsync(url))
             using (var content = response.Content)
             {
                 var xhtml = await content.ReadAsStringAsync();
 
-                // parse the XHTML result
+                // Parse the XHTML result
                 var doc = new HtmlDocument();
                 doc.LoadHtml(xhtml);
 
+                // Look for the <select> element with name catOid
                 var select = doc.DocumentNode.SelectSingleNode("//select[@id='catOid']");
+
+                // Grab all <option> children with a value not equal to zero
+                // The <option> value attribute contains the ID
+                // Inner node text of the element contains the category name
                 foreach (var child in select.SelectNodes("//option[not(@value='0')]"))
                 {
                     var optValue = child.Attributes["value"].Value;
